848a2073c06d9242dac4a0b6ff5e8d9fdbd91dc0
tolltech <paul@skbkontur.ru>
2017-04-01 10:57:40 +0400

    j1 work

--- JsonConversion/JsonProgram.cs
+++ JsonConversion/JsonProgram.cs
@@ -1,17 +1,64 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace JsonConversion
 {
-	class JsonProgram
-	{
-		static void Main()
-		{
-			string json = Console.In.ReadToEnd();
-			JObject v2 = JObject.Parse(json);
-			//...
-			var v3 = "{ 'version':'3', 'products': 'TODO' }";
-			Console.Write(v3);
-		}
-	}
+    class JsonProgram
+    {
+        static void Main()
+        {
+            var json = Console.In.ReadToEnd();
+
+            var serializer = new JsonSerializer
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            var before = JsonConvert.DeserializeObject<JsonV2>(json);
+            var products = Convert(before.Products);
+            using (var writer = new JsonTextWriter(Console.Out))
+                serializer.Serialize(writer, new JsonV3 { Version = "3", Products = products });
+
+            Console.ReadKey(true);
+        }
+
+        private static IEnumerable<ItemV3> Convert(IDictionary<int, ItemV2> products)
+        {
+            return
+                products.Select(
+                    kvp =>
+                        new ItemV3 { Id = kvp.Key, Name = kvp.Value.Name, Count = kvp.Value.Count, Price = kvp.Value.Price });
+        }
+    }
+
+    class ItemV2
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; }
+    }
+
+    class ItemV3
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; }
+    }
+
+    class JsonV2
+    {
+        public string Version { get; set; }
+        public IDictionary<int, ItemV2> Products { get; set; }
+    }
+
+    class JsonV3
+    {
+        public string Version { get; set; }
+        public IEnumerable<ItemV3> Products { get; set; }
+    }
 }
